4fd2b00d2c88b65ed1e285f49829938b65328df8
skhitaridze <skhitaridze@tbcbank.com.ge>
2023-03-25 16:13:57 +0400

    Animation manager modified to protocol oriented. Created worker path for future usage. Separated player model.

---
+++ Pigobana/Model/Player.swift
@@ -0,0 +1,17 @@
+//
+//  Player.swift
+//  Pigobana
+//
+//  Created by Saba Khitaridze on 13.02.23.
+//
+
+import UIKit
+
+enum Section {
+    case playerHolder
+}
+
+enum Player {
+    case first
+    case second
+}
